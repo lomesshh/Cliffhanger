45e76331c6400fccc15de06beaab82db9db2c553
Shrey Pandey <shreypandey27@gmail.com>
2022-05-07 11:20:08 +0530

    styling updates

--- src/frontend/routes/index.js
+++ src/frontend/routes/index.js
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Applicant from '../pages/applicant';
+import Landing from '../pages/landing';
 
 // Routes
+export const LANDING = '/';
 export const APPLICANT = '/applicant';
 
 export const availableRoutes = (
   <Routes>
+    <Route path={LANDING} element={<Landing />} />
     <Route path={APPLICANT} element={<Applicant />} />
   </Routes>
 );
